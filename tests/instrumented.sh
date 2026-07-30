f2e3e187805f08f9b944878e9fce9ee7bd79ef3c
hefengfan0615 <hefengfan0615@users.noreply.github.com>
2026-07-30 04:54:36 +0000

    Change go depth from 10 to 20 in instrumented test script

--- tests/instrumented.sh
+++ tests/instrumented.sh
@@ -67,7 +67,7 @@ esac
 # simple command line testing
 for args in "eval" \
             "go nodes 1000" \
-            "go depth 10" \
+            "go depth 20" \
             "go movetime 1000" \
             "go wtime 8000 btime 8000 winc 500 binc 500" \
             "bench 128 $threads 8 default depth"
@@ -98,7 +98,7 @@ cat << EOF > game.exp
  expect "bestmove"
 
  send "position fen 5rk1/1K4p1/8/8/3B4/8/8/8 b - - 0 1\n"
- send "go depth 10\n"
+ send "go depth 20\n"
  expect "bestmove"
 
  send "quit\n"
